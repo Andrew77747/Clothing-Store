c51ad99b8f21e5765dc7f7b9619f175927629c8e
Andrew77747 <andrew-walker@yandex.ru>
2022-03-29 13:29:28 +0300

    First commit

---
+++ ClothingStore/ClothingStore.Framework/Tools/IWebDriverManager.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace WebShop.Tricentis.Framework.Tools
+{
+    public interface IWebDriverManager : IDisposable
+    {
+        IWebDriver GetDriver();
+        WebDriverWait GetWaiter();
+    }
+}
